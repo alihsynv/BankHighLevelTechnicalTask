05d8f57229c31d7d02f532dc3955da350addbf83
Ali Hüseyn <ali.hsynov@gmail.com>
2025-09-11 20:59:03 +0400

    User and Phone entity updated

--- src/com/electronbank/domain/entity/User.java
+++ src/com/electronbank/domain/entity/User.java
@@ -1,4 +1,14 @@
 package com.electronbank.domain.entity;
 
+import com.electronbank.domain.value.Phone;
+
+import java.time.LocalDate;
+
 public class User {
+    private String fin;
+    private String password;
+    private String name;
+    private String surname;
+    private LocalDate dateOfBirth;
+    private Phone phone;
 }
